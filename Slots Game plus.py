7d97ac132c3fac921dba1fda62590150a005a112
jamesstanleyblack <37456736+jamesstanleyblack@users.noreply.github.com>
2018-03-16 16:10:47 -0500

    Add files via upload

---
+++ Slots Game plus.py
@@ -0,0 +1,70 @@
+#
+# Slot Machine
+#
+######################################################
+#
+print("Welcome to the Slot Machine!")
+numberOfTimes = input('How many times do you want to play?')
+slotsPossible = ["bar","bar","bar","cherry","crown"]
+Loser = ["\nFEED ME MORE MONEY", "\nYa lose", "\nThanks dood", "\nHow's that retirement fund looking?"]
+initialMoney = 100.0
+cherryWin = 100.0
+crownWin = 50.0
+barWin = 5.0
+#
+# This part builds an array of possible answers.
+# (the are also called "elements" of the array.
+# You can try adding or removing things to this list.
+#
+######################################################
+#
+from random import *
+#
+# This part loads random so we can use choice later.
+# Imports are a really neat part of Python, and we'll
+# talk more about them later, but for now, just know that
+# we're getting all of the things that random has.
+# (that's what the asterick does, grabs all of the things.)
+#
+######################################################
+#
+def play(initialMoney):
+    slot1=choice(slotsPossible)
+    slot2=choice(slotsPossible)
+    slot3=choice(slotsPossible)
+    sassyLoss = choice(Loser)
+    win = sassyLoss
+    
+    initialMoney = initialMoney-10.0
+    
+    #
+    # Here we set up three slot by naming three variables as
+    # slot1, slot2, and slot3.  If we wanted more, we would just
+    # need a slot4, slot5, and so on.
+    #
+    # the choice() function is from random that we imported
+    # above and that function will get one (and only one) of
+    # the possible elements on the array we built above.
+    #
+    ######################################################
+    #
+    if (slot1==slot2==slot3=="cherry"):
+        win = "\nYou win $100"
+    if (slot1==slot2==slot3=="crown"):
+        win = "\nYou win $50"
+    if (slot1==slot2==slot3=="bar"):
+        win = "\nYou win $5"
+    print (slot1+":"+slot2+":"+slot3+" "+win+" "+str(initialMoney+))
+    return initialMoney
+    #
+    # Here is where we decide if you won.  We check to see
+    # if the three slot variables are all equivilant (remember
+    # that you want equilivant (==) not an assignment (=).
+    # when they are all equilivant and match the last string, then
+    # we do something with it.  In this case, we return it to be printed.
+    #
+    ######################################################
+for i in range(int(numberOfTimes)):
+    initialMoney = int(play(initialMoney))
+    
+#we should add a tracker for winnings
